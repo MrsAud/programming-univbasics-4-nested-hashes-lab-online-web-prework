33f290791daf7ee3aa19f8da0a51514bc267fa2f
Audrea Cook <audreacwork@gmail.com>
2019-07-05 15:11:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  puts programmer_hash[:dennis_ritchies_language][:languages][0]
+  puts programmer_hash[:dennis_ritchies_language][:languages]
 end
 
 def adding_matz
